b2644298e5170d7b836d7e17da62e52bf1d4d253
Adrian Sawatzky <adrisawa188@ed.amdsb.ca>
2022-03-03 10:03:24 -0500

    Added remove button using lambda expression. Worked on search button

--- ClassInventory/Form1.cs
+++ ClassInventory/Form1.cs
@@ -13,6 +13,7 @@ namespace ClassInventory
     public partial class Form1 : Form
     {
         // TODO - create a List to store all inventory objects
+        List <Players> players = new List <Players> (); 
 
         public Form1()
         {
@@ -21,13 +22,20 @@ namespace ClassInventory
 
         private void addButton_Click(object sender, EventArgs e)
         {
-            // TODO - gather all information from screen 
+            // TODO - gather all information from screen
+            int age = Convert.ToInt32(ageInput.Text);
+            string playerName = nameInput.Text;
+            string team = teamInput.Text;
+            string position = positionInput.Text;   
 
             // TODO - create object with gathered information
+            Players p = new Players(age, playerName, team, position); 
 
             // TODO - add object to global list
+            players.Add(p);
 
             // TODO - display message to indicate addition made
+            outputLabel.Text = "Player added";
         }
 
         private void removeButton_Click(object sender, EventArgs e)
@@ -35,10 +43,27 @@ namespace ClassInventory
             // This is to be completed in Part II. You will use 
             // Lambda Expressions.
             //---------------------------
+            string playerName = removeInput.Text;
+            Players player = players.Find(x => x.playerName == removeInput.Text);
 
             // TODO - if object is in list remove it
+            if (players.Contains(player))
+            {
+                players.Remove(player);
+            }
 
             // TODO - display message to indicate deletion made
+            outputLabel.Text = "Player removed";
+
+            //foreach (Players player in players)
+            //{
+            //    if (playerName == player.playerName)
+            //    {
+            //        players.Remove(player);
+            //        outputLabel.Text = "Player removed";
+            //        break;
+            //    }
+            //}
         }
 
         private void searchButton_Click(object sender, EventArgs e)
@@ -46,14 +71,30 @@ namespace ClassInventory
             // This is to be completed in Part II. You will use 
             // Lambda Expressions.
             //---------------------------
+            string playerName = nameSearchInput.Text;
+            Players player = players.Find(x => x.playerName == nameSearchInput.Text);
 
             // TODO - if object entered exists in list show it
+            if (players.Contains(player))
+            {
+                outputLabel.Text = $"{playerName}"; 
+            }
             // TODO - else show not found message
+            else
+            {
+                outputLabel.Text = "Player not found";
+            }    
         }
 
         private void showButton_Click(object sender, EventArgs e)
         {
             // TODO - show all objects in list. Use a foreach loop.
+            outputLabel.Text = "";
+
+            foreach (Players player in players)
+            {
+                outputLabel.Text += $"{player.playerName} {player.age} {player.team} {player.position}\n";
+            }
         }
     }
 }
---
+++ ClassInventory/Players.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInventory
+{
+    internal class Players
+    {
+        //Name, age, team, position
+        public int age;
+        public string playerName;
+        public string team;
+        public string position;
+
+        public Players (int _age, string _name, string _team, string _position)
+        {
+            age = _age;
+            playerName = _name;
+            team = _team;
+            position = _position;
+        }
+    }
+}
